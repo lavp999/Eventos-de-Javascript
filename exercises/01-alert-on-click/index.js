0aac429345159445de7a1de3ccc54dcc3f00fa34
lavp999 <lavp99@hotmail.com>
2022-10-29 21:05:58 +0000

    El 2 sin completar

--- exercises/01-alert-on-click/index.js
+++ exercises/01-alert-on-click/index.js
@@ -1,3 +1,9 @@
 window.myClickFunction = function myClickFunction() {
 	alert("Your first function!");
 };
+
+let miBoton = document.getElementById("button2");
+// miBoton.addEventListener("onclick", myClickFunction);
+miBoton.setAttribute("onclick", "myClickFunction()");
+
+
--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,1 +1,8 @@
 //Declare your function here
+function miFuncion(){
+    alert("Hello World");
+}
+
+let miInput = document.getElementById("hello");
+miInput.setAttribute("onclick", "miFuncion()");
+miInput.setAttribute("value", "Hola Mundo");
