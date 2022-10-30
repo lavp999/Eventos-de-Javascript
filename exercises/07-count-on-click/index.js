2a0ddbcfb36182d775e2f88f2ec0b71c8efb9ed4
lavp999 <lavp99@hotmail.com>
2022-10-30 07:55:56 +0000

    pruebas!!! he quitado el botón de incrementar y sigue salidno

--- exercises/07-count-on-click/index.js
+++ exercises/07-count-on-click/index.js
@@ -1,23 +1,23 @@
 //This is a global variable
 var counter = 0;
 
-function muestraValor(){
-	return document.getElementById('screen').innerHTML = "The counter value is "+counter;
+function muestraValor(accion){
+	return document.getElementById('screen').innerHTML = "The counter value is "+counter+accion;
 }
 
 window.onload = function loadFunction()
 {
 	//here I set the screen to the initial value when the website is fully loaded.
-	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+	muestraValor(".");
 }
 
 //called when the user clicks
-window.increaseCounter = function increaseCounter()
+window.increaseCounter = function incrementa()
 {
 	//increase the global counter in one
 	counter++;
 	//update the screen with the new value
-	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+	muestraValor(". Añadiendo");
 } 
 
 window.decreaseCounter = function decreaseCounter()
@@ -25,7 +25,7 @@ window.decreaseCounter = function decreaseCounter()
 	//decrease the global counter in one
 	counter--;
 	//update the screen with the new value
-	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+	muestraValor(". Restando");
 } 
 
 
