0596d4808fc5c6cf947614393c692adb5470cd34
lavp999 <lavp99@hotmail.com>
2022-10-30 12:47:29 +0000

    hasta 7.1 un tanto peculiar...

--- exercises/07.1-change-turn-on-click/index.js
+++ exercises/07.1-change-turn-on-click/index.js
@@ -1,15 +1,28 @@
 var currentUser = "Mario";
+const jugadores = ["Mario", "Juan", "Josh"];
+const long = jugadores.length-1;
 
 window.onload = function loadfn() {
 	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
 }
 
+function encuentraSiguiente(actual){
+	let i = 0;
+
+	do {
+		console.log(jugadores[i], i, actual);
+		if (jugadores[i] == actual){
+			break;
+		}
+		i += 1;
+  	} while (i <= long);
+	if(i >= long)	return jugadores[0];
+	else			return jugadores[i+1];
+
+}
+
 window.turnChanger = function turnChanger() {
-	if (currentUser == "Mario") {
-		currentUser = "Juan";
-	} else {
-		currentUser = "Mario";
-	}
+	currentUser = encuentraSiguiente(currentUser);
 
 	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
 }
