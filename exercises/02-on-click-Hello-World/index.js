8c868156c5c99423a1d7af36284b391929553d0e
lavp999 <lavp99@hotmail.com>
2022-10-31 10:36:22 +0000

    Finalizada

--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,8 +1,7 @@
 //Declare your function here
 function laFuncion(){
     alert("Hello World");
-    return 0;
 }
 
 let miInput = document.getElementById("hello");
-miInput.addEventListener("click", laFuncion());
+miInput.addEventListener("click", laFuncion);
--- exercises/08-event-target/index.js
+++ exercises/08-event-target/index.js
@@ -2,7 +2,7 @@ window.onload = function loadFn() {
 	var containerElm = document.getElementById("container");
 	containerElm.addEventListener("click", function(event) {
 		//you code here
-		let targetElemento = event.target.textContent;	
+		let targetElemento = event.target.id;	
 		console.log(targetElemento);
 		alert(targetElemento);
 	});
